d8be95bc2dc33666efb582b75647699b1f54e23c
Karan <70339657+krn1904@users.noreply.github.com>
2022-07-17 00:15:51 +0530

    Time: 3 ms (81.49%), Space: 54.8 MB (45.16%) - LeetHub

---
+++ 645-set-mismatch/645-set-mismatch.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int i = 0;
+        while(i < nums.length){
+            int check = nums[i] - 1;
+            if(nums[i] != nums[check]){
+                swap(nums,i,check);
+            }else{
+                i++;
+            }
+        }
+        int[]  arr = new int[]{-1,-1};
+        for(int j = 0; j < nums.length; j++){
+            if(nums[j] != j+1){
+             arr[0] = nums[j];
+                arr[1] = j+1;
+            }
+        }
+        return arr;
+    }
+     public void swap(int[] nums,int start, int end){
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+    }
+    }
