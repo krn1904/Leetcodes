54e7c183f383f2a7d5120cd67a7079995232df68
Karan <70339657+krn1904@users.noreply.github.com>
2022-08-28 16:50:07 +0530

    Time: 1048 ms (13.73%), Space: 46.8 MB (34.55%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,11 +1,23 @@
 class Solution {
     public boolean isPalindrome(String s) {
         String ns = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();//replaces all the special character with ""
-        StringBuilder res = new StringBuilder(ns);
-        res = res.reverse();
-        if(res.toString().equals(ns)){
-        return true;
+        // StringBuilder res = new StringBuilder(ns);
+        // res = res.reverse();
+        int i =0;
+        int j =ns.length()-1;
+         while (i <= j) {
+            if (ns.charAt(i) == ns.charAt(j)) {
+                i++;
+                j--;
+            } else {
+                return false;
+            }
         }
-        return false;
+        return true;
+        //first submit...
+        // if(res.toString().equals(ns)){
+        // return true;
+        // }
+        // return false;
     }
 }
